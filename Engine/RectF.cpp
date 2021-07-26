020345fbd9749513280682164f05ae5701179792
Juuya <Stridin@protonmail.com>
2021-07-26 23:15:39 +0200

    Revised RectF to not give a shit about colors

--- Engine/RectF.cpp
+++ Engine/RectF.cpp
@@ -1,33 +1,32 @@
 #include "RectF.h"
 
-RectF::RectF(const float left, const float top, const float right, const float bottom, const Color color)
+RectF::RectF(const float left, const float top, const float right, const float bottom)
 	:
 	left(left),
 	top(top),
 	right(right),
-	bottom(bottom),
-	color(color)
+	bottom(bottom)
 {
 }
 
-RectF::RectF(const Vec2& top_left, const Vec2& bottom_right, const Color color)
+RectF::RectF(const Vec2& top_left, const Vec2& bottom_right)
 	:
-	RectF(top_left.x, top_left.y, bottom_right.x, bottom_right.y, color)
+	RectF(top_left.x, top_left.y, bottom_right.x, bottom_right.y)
 {
 }
 
-RectF::RectF(const Vec2& top_left, const float width, const float height, const Color color)
+RectF::RectF(const Vec2& top_left, const float width, const float height)
 	:
-	RectF(top_left, Vec2(width, height), color)
+	RectF(top_left, Vec2(width, height))
 {
 }
 
-void RectF::fromCenter(const Vec2& center, const float half_width, const float half_height, const Color color)
+RectF RectF::fromCenter(const Vec2& center, const float half_width, const float half_height)
 {
-	RectF(center.x - half_width, center.y - half_height, center.x + half_width, center.y + half_height, color);
+	return RectF(center.x - half_width, center.y - half_height, center.x + half_width, center.y + half_height);
 }
 
-void RectF::Draw(Graphics& gfx) const
+void RectF::Draw(Graphics& gfx, Color color) const
 {
 	gfx.DrawRect( int( left ), int( top ), int( right ), int( bottom ), color );
 }
--- Engine/RectF.h
+++ Engine/RectF.h
@@ -1,21 +1,19 @@
 #pragma once
 
 #include "Vec2.h"
-#include "Colors.h"
 #include "Graphics.h"
 
 class RectF
 {
 public:
-	RectF( const float left, const float top, const float right, const float bottom, const Color color );
-	RectF( const Vec2& top_left, const Vec2& bottom_right, const Color color );
-	RectF( const Vec2& top_left, const float width, const float height, const Color color );
-	static void fromCenter( const Vec2& center, const float half_width, const float half_height, const Color color );
-	void Draw(Graphics& gfx) const;
+	RectF( const float left, const float top, const float right, const float bottom);
+	RectF( const Vec2& top_left, const Vec2& bottom_right);
+	RectF( const Vec2& top_left, const float width, const float height);
+	static RectF fromCenter( const Vec2& center, const float half_width, const float half_height);
+	void Draw(Graphics& gfx, const Color color) const;
 public:
 	float left;
 	float top;
 	float right;
 	float bottom;
-	Color color;
 };
