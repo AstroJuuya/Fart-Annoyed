4bee07e776e9afa28cc5070dd4aab50faf79cb85
Juuya <AstroJuuya@protonmail.com>
2021-07-30 20:29:10 +0200

    Added wall collision handling to the Ball

--- Engine/Ball.cpp
+++ Engine/Ball.cpp
@@ -7,6 +7,11 @@ Ball::Ball(Vec2 pos, Vec2 vel)
 {
 }
 
+void Ball::Update(const float delta_time)
+{
+	pos += vel * delta_time;
+}
+
 void Ball::ReboundX()
 {
 	vel.x = -vel.x;
@@ -22,6 +27,31 @@ void Ball::ReboundY()
 void Ball::Draw(Graphics& gfx) const
 {
 	SpriteCodex::DrawBall(pos, gfx);
+}
+
+void Ball::DoWallCollision(RectF walls)
+{
+	if (pos.x - radius < walls.left)
+	{
+		pos.x += walls.left - ( pos.x - radius );
+		ReboundX();
+	}
+	else if (pos.x + radius >= walls.right)
+	{
+		pos.x += walls.right - ( pos.x + radius );
+		ReboundX();
+	}
+
+	if (pos.y - radius < walls.top)
+	{
+		pos.y += walls.top - (pos.y - radius);
+		ReboundY();
+	}
+	else if (pos.y + radius >= walls.bottom)
+	{
+		pos.y += walls.bottom - (pos.y + radius);
+		ReboundY();
+	}
 }
 
 RectF Ball::GetRect() const
--- Engine/Ball.h
+++ Engine/Ball.h
@@ -3,14 +3,17 @@
 #include "Graphics.h"
 #include "RectF.h"
 #include "SpriteCodex.h"
+#include "FrameTimer.h"
 
 class Ball
 {
 public:
 	Ball(Vec2 pos, Vec2 vel);
+	void Update(const float delta_time);
 	void ReboundX();
 	void ReboundY();
 	void Draw(Graphics& gfx) const;
+	void DoWallCollision(RectF walls);
 	RectF GetRect() const;
 	bool HasCollided() const;
 private:
