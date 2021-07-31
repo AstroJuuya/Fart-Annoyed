d969fb324a1585679ec40cfd8edbe5dc6294f9a8
Juuya <AstroJuuya@protonmail.com>
2021-07-31 21:54:05 +0200

    Added standard constructor to RectF

--- Engine/RectF.h
+++ Engine/RectF.h
@@ -6,6 +6,7 @@
 class RectF
 {
 public:
+	RectF() = default;
 	RectF( const float left, const float top, const float right, const float bottom);
 	RectF( const Vec2& top_left, const Vec2& bottom_right);
 	RectF( const Vec2& top_left, const float width, const float height);
