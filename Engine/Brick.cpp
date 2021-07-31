3db39e41b70cd0a82334a0713e595c13160d3f22
Juuya <AstroJuuya@protonmail.com>
2021-07-31 20:27:24 +0200

    Made ball able to collide with Bricks

--- Engine/Brick.cpp
+++ Engine/Brick.cpp
@@ -8,9 +8,19 @@ Brick::Brick(const RectF& rect)
 
 void Brick::DoBallCollision(Ball& ball)
 {
-	if ( IsOverlappingWith( ball.GetRect() ) && !ball.HasCollided() )
+	if (IsOverlappingWith(ball.GetRect()) && !ball.HasCollided() )
 	{
-		ball.ReboundY();
+		RectF b = ball.GetRect();
+		if (	( abs(b.right - brick.left) <= abs(b.bottom - brick.top) && abs(b.right - brick.left) <= abs(b.top - brick.bottom) ) 
+			||	( abs(b.left - brick.right) <= abs(b.bottom - brick.top) && abs(b.left - brick.right) <= abs(b.top - brick.bottom) ) )
+		{
+			ball.ReboundX();
+		}
+		if (	( abs(b.bottom - brick.top) <= abs(b.right - brick.left) && abs(b.bottom - brick.top) <= abs(b.left - brick.right) )
+			||	( abs(b.top - brick.bottom) <= abs(b.right - brick.left) && abs(b.top - brick.bottom) <= abs(b.left - brick.right) ) )
+		{
+			ball.ReboundY();
+		}
 		destroyed = true;
 	}
 }
@@ -23,5 +33,5 @@ void Brick::Draw(Graphics& gfx, Color color) const
 bool Brick::IsOverlappingWith(const RectF& other) const
 {
 	return brick.left < other.right && brick.top < other.bottom
-		&& brick.right < other. left && brick.bottom < other.top;
+		&& brick.right > other. left && brick.bottom > other.top;
 }
